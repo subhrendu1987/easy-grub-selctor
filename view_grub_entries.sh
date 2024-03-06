84b24960e6d9c24413ce040e1db3d88f0739c1d3
subhrendu1987 <subhrendu@idrbt.ac.in>
2024-03-06 06:34:26 +0530

    deleted:    Ubuntu, with Linux 5.15.0-91-generic 	deleted:    Ubuntu, with Linux 5.15.0-91-generic (recovery mode) 	deleted:    Ubuntu, with Linux 5.15.0-94-generic 	deleted:    Ubuntu, with Linux 5.15.0-94-generic (recovery mode) 	deleted:    Ubuntu, with Linux 5.15.0-97-generic 	deleted:    Ubuntu, with Linux 5.15.0-97-generic (recovery mode) 	modified:   view_grub_entries.sh

--- view_grub_entries.sh
+++ view_grub_entries.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
-### Example GRUB_DEFAULT="Advanced options for Ubuntu>Ubuntu, with Linux 5.15.0-97-generic"
+
+# Function to find the default entry in GRUB
+find_default_entry() {
+    # Check if the GRUB_DEFAULT variable is set in /etc/default/grub
+    if grep -q "^GRUB_DEFAULT=" /etc/default/grub; then
+        # Extract the value of GRUB_DEFAULT
+        DEFAULT_ENTRY=$(grep "^GRUB_DEFAULT=" /etc/default/grub | cut -d'"' -f2)
+        # Parse the output of display_grub_boot_entries to find the serial number of the default entry
+        SERIAL_NUMBER=$(echo "$GRUB_ENTRIES" | grep -F "=\"$DEFAULT_ENTRY\"" | awk '{print $1}')
+        if [ -n "$SERIAL_NUMBER" ]; then
+            echo "[Note] Default GRUB Entry: $DEFAULT_ENTRY (Matching Serial Number(s): $SERIAL_NUMBER)"
+        else
+            echo "[Note] Default GRUB Entry: $DEFAULT_ENTRY (Serial Number: Not Found)"
+        fi
+    else
+        echo "GRUB_DEFAULT is not set in /etc/default/grub."
+    fi
+}
+
 # Function to display GRUB entries
 display_grub_boot_entries() {
+GRUB_ENTRIES=$(awk -F\' '/^menuentry / {printf "\t%d. GRUB_DEFAULT=\"%s\"\n", ++count, $2}
+                  /^submenu / {submenu=$2; subcount=0; ++count}
+                  /^\tmenuentry / {printf "\t\t%d>%d. GRUB_DEFAULT=\"%s>%s\"\n", count, ++subcount, submenu, $2}' /boot/grub/grub.cfg)
+
     echo "GRUB Boot Entries:"
-    awk -F\' '/^menuentry / {printf "\t%d. GRUB_DEFAULT=\"%s\"\n", ++count, $2}
-              /^submenu / {submenu=$2}
-              /^\tmenuentry / {printf "\t\t%d. GRUB_DEFAULT=\"%s>%s\"\n", ++count, submenu, $2}' /boot/grub/grub.cfg
+    echo "$GRUB_ENTRIES"
 }
 
 # Check if the script is run with root privileges
@@ -15,7 +35,9 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # Call function to display GRUB entries
-echo "Modify /etc/default/grub file with the suitable entries" 
 display_grub_boot_entries
-echo "sudo update-grub" 
 
+echo "--------------"
+echo "[Note] Modify /etc/default/grub file with the suitable entries"
+find_default_entry
+echo "Update grub with sudo update-grub"
